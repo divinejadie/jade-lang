84d940d4b7e9b82013266b6e9a1604e48128ca6e
Amelia Joison <joison.me@gmail.com>
2022-11-15 21:19:00 +1100

    Fix Warning: Allow dead code for common testing func

--- tests/common/mod.rs
+++ tests/common/mod.rs
@@ -1,5 +1,6 @@
 use jadescript::JitCodegen;
 
+#[allow(dead_code)]
 pub fn run_code<I, O>(jit: &mut JitCodegen, code: &str, input: I) -> Result<O, String> {
     let code_ptr = jit.compile(code)?;
     let code_fn = unsafe { std::mem::transmute::<_, fn(I) -> O>(code_ptr) };
