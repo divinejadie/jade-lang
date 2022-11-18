e4c7c12286696c4819c598d0bf710969afb573f6
Amelia Joison <joison.me@gmail.com>
2022-11-18 23:07:45 +1100

    Tests: Add test for running functions lexically not yet declared

--- tests/function.rs
+++ tests/function.rs
@@ -37,6 +37,16 @@ fn main(input: i32) -> i32:
 	return until_zero(input)
 "#;
 
+const OUT_OF_ORDER: &str = r#"
+fn main(a: f32) -> f32:
+	let mut x: f32 = a
+	x = x.double()
+	return x
+
+fn double(x: f32) -> f32:
+	return x * 2.0
+"#;
+
 #[test]
 fn function_call() {
     let mut jit_codegen = JitCodegen::default();
@@ -70,6 +80,15 @@ fn recursion() {
     assert_eq!(
         common::run_code::<i32, i32>(&mut jit_codegen, RECURSION, 10).unwrap(),
         0
+    );
+}
+
+#[test]
+fn out_of_order() {
+    let mut jit_codegen = JitCodegen::default();
+    assert_eq!(
+        common::run_code::<f32, f32>(&mut jit_codegen, OUT_OF_ORDER, 10.0).unwrap(),
+        20.0
     );
 }
 
