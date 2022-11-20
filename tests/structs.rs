b6ba87150bee7f760677cff38ddfc1822c83d883
Amelia Joison <joison.me@gmail.com>
2022-11-20 22:07:42 +1100

    Add tests for binding a variable to a struct instance

--- tests/structs.rs
+++ tests/structs.rs
@@ -13,9 +13,45 @@ fn main() -> bool:
 	return true
 "#;
 
+const STRUCT_VAR_BIND: &str = r#"
+struct Test:
+	member_var_1: i32,
+	member_var_2: f32
+
+fn main() -> bool:
+	let x: Test = Test:
+		member_var_1: 1,
+		member_var_2: 2.0
+	return true
+"#;
+
+const STRUCT_VAR_BIND_NO_HINT: &str = r#"
+struct Test:
+	member_var_1: i32,
+	member_var_2: f32
+
+fn main() -> bool:
+	let x = Test:
+		member_var_1: 1,
+		member_var_2: 2.0
+	return true
+"#;
+
 #[test]
 fn struct_instantiate() {
     let mut jit_codegen = JitCodegen::default();
-    assert!(common::run_code::<(), bool>(&mut jit_codegen, STRUCT_INST, ()).unwrap(),);
+    assert!(common::run_code::<(), bool>(&mut jit_codegen, STRUCT_INST, ()).unwrap());
+}
+
+#[test]
+fn struct_variable_binding_type_hint() {
+    let mut jit_codegen = JitCodegen::default();
+    assert!(common::run_code::<(), bool>(&mut jit_codegen, STRUCT_VAR_BIND, ()).unwrap());
+}
+
+#[test]
+fn struct_variable_binding_no_type_hint() {
+    let mut jit_codegen = JitCodegen::default();
+    assert!(common::run_code::<(), bool>(&mut jit_codegen, STRUCT_VAR_BIND_NO_HINT, ()).unwrap());
 }
 
